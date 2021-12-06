b1f56e0a6066ee8af0e3cb6507a3d76b6f6077b2
Kevin Wu <kevin@kevinniuwu.com>
2021-12-06 00:23:07 -0500

    Add day 6

---
+++ day_6/day_6.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from icecream import ic
+from collections import defaultdict
+
+local_dir = os.path.dirname(__file__)
+sys.path.append(os.path.join(local_dir, ".."))
+from aoc import run
+
+
+def calc_population(fish: list[int], days: int) -> int:
+    pop: defaultdict[int, int] = defaultdict(int)
+    for n in fish:
+        pop[n] += 1
+
+    for _ in range(days):
+        new_dict = defaultdict(int)
+        for k, v in pop.items():
+            new_dict[k - 1] = v
+
+        if -1 in new_dict:
+            new_dict[8] = new_dict[-1]
+            new_dict[6] += new_dict[-1]
+            new_dict[-1] = 0
+
+        pop = new_dict
+
+    return sum(pop.values())
+
+
+def part1(data: str):
+    fish = list(map(int, data.split(",")))
+    return calc_population(fish, 80)
+
+
+def part2(data: str):
+    fish = list(map(int, data.split(",")))
+    return calc_population(fish, 256)
+
+
+run(part1, part2, os.path.join(local_dir, "input.txt"))
+# run(part1, part2, """""")
